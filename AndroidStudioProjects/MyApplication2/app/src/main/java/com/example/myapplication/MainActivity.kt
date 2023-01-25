090c32d9de6a8ba0b6286c9b4a90dddea6701b90
Soumyodip Sutradhar <74486329+Samsand-5@users.noreply.github.com>
2023-01-25 20:37:55 +0530

    Update MainActivity.kt

--- AndroidStudioProjects/MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ AndroidStudioProjects/MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,96 +1,136 @@
-package com.example.myapplication;
+package com.example.bluetoothconnec;
 
-import android.os.Bundle
-import android.view.View
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.drm.DrmStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_ENABLE_BT = 0;
+    private static final int REQUEST_DISCOVER_BT = 1;
+    private Set<BluetoothDevice> devices;
+    TextView mstatus, pairedtv;
+    Button monbtn;
+    Button moffbtn;
+    Button mdisco;
+    Button mpaired;
+    private BluetoothAdapter mblueadapter;
+    private Set<BluetoothDevice> pairedDevices;
 
 
-/**
- * This activity keeps track of the basketball score for 2 teams.
- */
-class MainActivity : AppCompatActivity() {
-    // Tracks the score for Team A
-    var scoreTeamA = 0
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mstatus = findViewById(R.id.status);
+        monbtn = findViewById(R.id.onbtn);
+        moffbtn = findViewById(R.id.offbtn);
+        mdisco = findViewById(R.id.disco);
+        mpaired = findViewById(R.id.paired);
+        mblueadapter = BluetoothAdapter.getDefaultAdapter();
 
-    // Tracks the score for Team B
-    var scoreTeamB = 0
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        if (mblueadapter == null) {
+            mstatus.setText("BLUETOOTH NOT AVAILABLE");
+        } else {
+            mstatus.setText("BLUETOOTH  AVAILABLE");
+        }
+        monbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mblueadapter == null) {
+                    Toast.makeText(getApplicationContext(), "Bluetooth Not Supported", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (!mblueadapter.isEnabled()) {
+                        Intent turnon = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                            startActivityForResult(turnon, 1);
+                            Toast.makeText(getApplicationContext(), "Bluetooth Turned ON", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        else {
+                            ActivityCompat.requestPermissions(MainActivity.this,
+                                    new String[]{Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN},
+                                    REQUEST_ENABLE_BT);
+                        }
+                    }
+                }
+            }
+        });
+        moffbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mblueadapter.isEnabled()) {
+                    if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                        mblueadapter.disable();
+                        showToast("TURNING OFF BLUETOOTH");
+                        return;
+                    }
+                    else {
+                        ActivityCompat.requestPermissions(MainActivity.this,
+                                new String[]{Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN},
+                                REQUEST_ENABLE_BT);
+                    }
+
+                } else {
+                    showToast("BLUETOOTH IS ALREADY OFF");
+                }
+
+            }
+        });
+
+        mdisco.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!mblueadapter.isEnabled()) {
+                    showToast("Making your device discovarable");
+                    Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+                }
+            }
+        });
+        mpaired.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                    Set<BluetoothDevice> devices = mblueadapter.getBondedDevices();
+                    for (BluetoothDevice device : devices) {
+                        pairedtv.append("\nDEVICES" + device.getName() + " ," + device);
+                    }
+                    return;
+                } else {
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN},
+                            REQUEST_ENABLE_BT);
+                    showToast("TURNING ON BLUETOOTH TO GET PAIRED DEVICES");
+                }
+            }
+        });
     }
 
-    /**
-     * Increase the score for Team A by 1 point.
-     */
-    fun addOneForTeamA(v: View?) {
-        scoreTeamA = scoreTeamA + 1
-        displayForTeamA(scoreTeamA)
+    protected void onActivityResult(int requestcode, int resultcode, Intent data){
+        if(resultcode==RESULT_OK){
+            //bluetooth is on
+            showToast("Bluetooth is on");
+        }
+        else{
+            //user denied to turn on bluetooth
+            showToast("cannot on bluetooth");
+        }
+        super.onActivityResult(requestcode, resultcode, data);
     }
 
-    /**
-     * Increase the score for Team A by 2 points.
-     */
-    fun addTwoForTeamA(v: View?) {
-        scoreTeamA = scoreTeamA + 2
-        displayForTeamA(scoreTeamA)
-    }
-
-    /**
-     * Increase the score for Team A by 3 points.
-     */
-    fun addThreeForTeamA(v: View?) {
-        scoreTeamA = scoreTeamA + 3
-        displayForTeamA(scoreTeamA)
-    }
-
-    /**
-     * Increase the score for Team B by 1 point.
-     */
-    fun addOneForTeamB(v: View?) {
-        scoreTeamB = scoreTeamB + 1
-        displayForTeamB(scoreTeamB)
-    }
-
-    /**
-     * Increase the score for Team B by 2 points.
-     */
-    fun addTwoForTeamB(v: View?) {
-        scoreTeamB = scoreTeamB + 2
-        displayForTeamB(scoreTeamB)
-    }
-
-    /**
-     * Increase the score for Team B by 3 points.
-     */
-    fun addThreeForTeamB(v: View?) {
-        scoreTeamB = scoreTeamB + 3
-        displayForTeamB(scoreTeamB)
-    }
-
-    /**
-     * Resets the score for both teams back to 0.
-     */
-    fun resetScore(v: View?) {
-        scoreTeamA = 0
-        scoreTeamB = 0
-        displayForTeamA(scoreTeamA)
-        displayForTeamB(scoreTeamB)
-    }
-
-    /**
-     * Displays the given score for Team A.
-     */
-    fun displayForTeamA(score: Int) {
-        val scoreView = findViewById<View>(R.id.team_a_score) as TextView
-        scoreView.text = score.toString()
-    }
-
-    /**
-     * Displays the given score for Team B.
-     */
-    fun displayForTeamB(score: Int) {
-        val scoreView = findViewById<View>(R.id.team_b_score) as TextView
-        scoreView.text = score.toString()
+    private void showToast(String ms) {
+        Toast.makeText(this, ms, Toast.LENGTH_SHORT).show();
     }
 }
